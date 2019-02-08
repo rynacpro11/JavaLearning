72a70ebbee55d4226132a15b5d70c86beb870014
Ryan Christensen <rynacpro+git@gmail.com>
2019-02-07 18:28:17 -0800

    stuff

--- CalcEngine/src/com/pluralsight/calcEngine/Main.java
+++ CalcEngine/src/com/pluralsight/calcEngine/Main.java
@@ -3,26 +3,31 @@ package com.pluralsight.calcEngine;
 public class Main {
 
     public static void main(String[] args) {
-	    double val1 = 100.0d;
-	    double val2 = 50.0d;
-	    double result;
-	    char opCode = 'd';
+    	double[] leftVals = {100.0d, 25.0d, 225.03, 11.0d};
+	    double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+	    char[] opCodes = {'d', 'a', 's', 'm'};
+	    double[] results = new double[opCodes.length];
 
-	    if (opCode=='a')
-	            result = val1 + val2;
-	    else if (opCode == 's')
-	            result =  val1 - val2;
-	    else if (opCode == 'd') {
-			result = val2 != 0.0d ? val1 / val2 : 0.0d;
-		}
+//		double val2 = 100.0d;
+//	    double val2 = 50.0d;
+//	    double result;
+//	    char opCode = 'd';
 
-	    else if (opCode == 'm')
-	            result = val1 + val2;
-	    else
-	    	{
+		for (int i = 0; i< opCodes.length; i++) {
+
+			if (opCodes[i] == 'a')
+				results[i] = leftVals[i] + rightVals[i];
+			else if (opCodes[i] == 's')
+				results[i] = leftVals[i] - rightVals[i];
+			else if (opCodes[i] == 'd') {
+				results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+			} else if (opCodes[i] == 'm')
+				results[i] = leftVals[i] + rightVals[i];
+			else {
 				System.out.println("Error - invalid opCode");
-				result = 0.0d;
+				results[i] = 0.0d;
 			}
+		}
 	    System.out.println(result);
     }
 }
