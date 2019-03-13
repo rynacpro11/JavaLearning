23601ea29650f6facbce3f31bae87e6efa37c65e
Ryan Christensen <rynacpro+git@gmail.com>
2019-03-12 17:57:31 -0700

    class inheritance

---
+++ CalcEngine/src/com/pluralsight/calcEngine/Multiplier.java
@@ -0,0 +1,19 @@
+package com.pluralsight.calcEngine;
+
+public class Multiplier extends CalculateBase {
+
+
+    public Multiplier() {}
+    public Multiplier (double leftVal, double rightVal){
+        super(leftVal, rightVal);
+    }
+
+
+    @Override
+    public void calculate(){
+        double value = getLeftVal() * getRightVal();
+        setResult(value);
+    }
+}
+
+
---
+++ CalcEngine/src/com/pluralsight/calcEngine/Subtracter.java
@@ -0,0 +1,16 @@
+package com.pluralsight.calcEngine;
+
+public class Subtracter extends CalculateBase {
+    public Subtracter() {}
+    public Subtracter (double leftVal, double rightVal){
+            super(leftVal, rightVal);
+    }
+
+
+    @Override
+    public void calculate(){
+        double value = getLeftVal() - getRightVal();
+        setResult(value);
+    }
+}
+
