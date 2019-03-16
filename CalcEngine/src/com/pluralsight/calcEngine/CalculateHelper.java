35d85638ae16937252f6158fd7e42d6d8543b5c5
Ryan Christensen <rynacpro+git@gmail.com>
2019-03-15 18:06:18 -0700

    new stuff March 15 exceptions

--- CalcEngine/src/com/pluralsight/calcEngine/CalculateHelper.java
+++ CalcEngine/src/com/pluralsight/calcEngine/CalculateHelper.java
@@ -12,14 +12,25 @@ public class CalculateHelper {
     double rightValue;
     double result;
 
-    public void process(String statement) {
+    public void process(String statement) throws InvalidStatementException {
         // add 1.0 2.0
         String[] parts = statement.split(" ");
-        String commandString = parts[0]; //add
-        leftValue = Double.parseDouble(parts[1]);  //1.0 as a double
-        rightValue = Double.parseDouble(parts[2]); //2.0 as a double
+        if (parts.length != 3)
+            throw new InvalidStatementException("Incorrect number of fields", statement);
 
-        setCommandFromString(commandString );
+
+
+        String commandString = parts[0]; //add
+
+        try {
+            leftValue = Double.parseDouble(parts[1]);  //1.0 as a double
+            rightValue = Double.parseDouble(parts[2]); //2.0 as a double
+        } catch (NumberFormatException e) {
+            throw new InvalidStatementException("Non-numeric data", statement, e);
+        }
+        setCommandFromString(commandString);
+        if (command == null)
+            throw new InvalidStatementException("Invalid command", statement);
 
         CalculateBase calculator = null;
 
